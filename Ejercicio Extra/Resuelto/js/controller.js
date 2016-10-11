4d1bc3776ad74845004ba54b1d54fafbb775a834
Patox <nmspatox@gmail.com>
2016-10-11 17:23:56 -0300

    Se agrega delete y localStorage

--- Ejercicio Extra/Resuelto/js/controller.js
+++ Ejercicio Extra/Resuelto/js/controller.js
@@ -1,12 +1,19 @@
 angular.module('booksApp')
 .controller('BooksCtrl', BooksCtrl);
 
-function BooksCtrl(BooksService) {
+function BooksCtrl($scope, $window, BooksService) {
 
-	this.allBooks = BooksService.allBooks();
+	this.allBooks = BooksService.allBooks();	
 
-	this.addBook = function(newBook){
-		BooksService.addBook(newBook);
-	}
+	this.addBook = function(newBook){					
+		BooksService.addBook(angular.copy(newBook));
+		
+		newBook.title = '';
+		newBook.author = '';		
+		$scope.newBookForm.$setPristine();
+	};
 
+	this.deleteBook = function(idx){
+		BooksService.deleteBook(idx);
+	};
 }
